d88f08696879f344c4458fd925b13332e7f5c56f
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-11-09 05:34:09 +0900

    update

---
+++ script/kidsup.js
@@ -0,0 +1,12 @@
+//https://api.kidsup.net/verify
+  
+var obj = JSON.parse($response.body);
+
+obj= {
+	"response": {
+		"status": true,
+		"message": "Thiết bị phù hợp"
+	}
+};
+
+$done({body: JSON.stringify(obj)});

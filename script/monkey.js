08c0c7febd4898bcca8a09105a673f33185ebbad
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-04-26 22:49:29 +0700

    Update monkey.js

--- script/monkey.js
+++ script/monkey.js
@@ -1,14 +1,18 @@
-var obj = JSON.parse($response.body);
+let obj = JSON.parse($response.body);
 
-obj.data.purchased=
-{
-			"pay_inapp": false,
-			"time_purchased": 0,
-			"time_activated": 1571981288,
-			"time_expire": 0,
-			"product_list": [],
-			"is_active": true,
-			"is_free": false
-		};
-obj.data.show_learn_to_spell=true;
+obj.data.purchased ={
+	"pay_inapp": true,
+	"time_purchased": 0,
+	"time_activated": 0,
+	"time_expire": 0,
+	"product_list": [],
+	"is_active": true,
+	"is_free": false,
+	"free_days": 999,
+	"is_purchased": true,
+	"purchased_packages": {
+		"iap": [],
+		"cod": []
+	}
+};
 $done({body: JSON.stringify(obj)});
